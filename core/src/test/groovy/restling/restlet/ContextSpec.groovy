a63a530e33975ae6ee9e0da846d68c5d3088caa1
Robert Fischer <robert.fischer@weboniselab.com>
2015-08-10 12:46:14 -0400

    Demonstrate that you can assert the context

---
+++ core/src/test/groovy/restling/restlet/ContextSpec.groovy
@@ -0,0 +1,15 @@
+package restling.restlet
+
+import org.restlet.Context
+import spock.lang.Specification
+
+/**
+ * Specification to check that {@link Context} works.
+ */
+class ContextSpec extends Specification {
+
+    def "fresh context can be asserted"() {
+        expect:
+        assert new Context(): "No context present!"
+    }
+}
